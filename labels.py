bf5c245e491dadb6948cbc0e4e5db0d05b95cc62
Allison Aprile <49654275+aaprile2@users.noreply.github.com>
2020-12-07 07:55:16 -0500

    Update labels.py
    Includes array feature

--- labels.py
+++ labels.py
@@ -7,6 +7,7 @@ Creating ground truth csvs for the training and testing images.
 # Imports
 import pandas as pd
 import glob
+import cv2
 
 # Path of cropped dataset
 dir = '/Users/aaprile/Desktop/cropped_archive'
@@ -16,26 +17,31 @@ train_file = []
 test_file = []
 train_label = []
 test_label = []
+train_array = []
+test_array = []
 
 # Create training file
 for label in glob.glob(dir + '/train/*'):
     for img in glob.glob(label + '/*'):
         train_file.append(img.split('/')[-1])
         train_label.append(label.split('/')[-1])
+        train_array.append(cv2.imread(img,0))
+        
 
 # Create testing file
 for label in glob.glob(dir + '/test/*'):
     for img in glob.glob(label + '/*'):
         test_file.append(img.split('/')[-1])
         test_label.append(label.split('/')[-1])
+        test_array.append(cv2.imread(img,0))
    
 # Create DataFrame
-train = pd.DataFrame(list(zip(train_file, train_label)), 
-               columns =['file_name', 'label'])
+train = pd.DataFrame(list(zip(train_file, train_array, train_label)), 
+               columns =['file_name', 'array', 'label'])
 
-test = pd.DataFrame(list(zip(test_file, test_label)),
-                    columns =['file_name', 'label'])
+test = pd.DataFrame(list(zip(test_file, test_array, test_label)),
+                    columns =['file_name', 'array', 'label'])
 
 # Export to csv
-train.to_csv(dir + '/train.csv', index=False)
-test.to_csv(dir + '/test.csv', index=False) 
+train.to_csv(dir + '/cropped_train.csv', index=False)
+test.to_csv(dir + '/cropped_test.csv', index=False) 
